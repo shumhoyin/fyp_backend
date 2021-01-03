4d9645f12e9a473f4cd016e34ffbb149e895ab76
ShumHoYin <19232837@life.hkbu.edu.hk>
2021-01-03 19:35:00 +0800

    1) minor change on database setting

--- DBConnection.js
+++ DBConnection.js
@@ -6,16 +6,17 @@ var mongoDB = "mongodb+srv://shumhoyin:m9uMPPhjXd0uuc2d@appcluster.hwh3s.mongodb
 const ConnectDB = () =>{
 
     mongoose.connect(mongoDB,{
-        useUnifiedTopology: true, 
-        useNewUrlParser: true
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex:true
      }).then(()=>{
-    
+
     console.log("Mongodb Connected ! ");
     // Get Mongoose to use the global promise library
     mongoose.Promise = global.Promise;
     //Get the default connection
     var db = mongoose.connection;
-    
+
     //Bind connection to error event (to get notification of connection errors)
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
